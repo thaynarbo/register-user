425d43e6d02f1d37f7ac2d79f912050cdaafa495
thaynar96 <thaynarbo@gmail.com>
2021-10-10 14:07:05 -0300

    Task Page

---
+++ src/pages/userTaks/index.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import CardTaskList from '../../components/Tasks/CardTaskList'
+import Button from '../../components/Button'
+import { useTask } from '../../context/taskContext'
+import ModalCreateTask from '../../components/Tasks/ModalCreateTask'
+import { Container } from './styles'
+import { BiArrowBack } from 'react-icons/bi'
+import ButonRadius from '../../components/ButtonRadius'
+import { Link } from 'react-router-dom'
+import ModalExcluirTask from '../../components/Tasks/ModalExcluirTask'
+
+const UserTask = () => {
+    let { id } = useParams()
+    const { tasks, getTasks, openModalTask } = useTask()
+
+    useEffect(() => {
+        getTasks(id)
+    }, [tasks])
+
+    return (
+        <Container>
+            <Link to={'/'} className={'back'}>
+                <ButonRadius icon={<BiArrowBack />} outline />
+                <p>Lista de Usuarios</p>
+            </Link>
+            <Button onClick={() => openModalTask()}>Cadastrar Tarefa</Button>
+            <div>
+                {tasks?.map((el) => {
+                    return <CardTaskList task={el} />
+                })}
+            </div>
+            <ModalCreateTask id={id} />
+            <ModalExcluirTask id={id} />
+        </Container>
+    )
+}
+
+export default UserTask
